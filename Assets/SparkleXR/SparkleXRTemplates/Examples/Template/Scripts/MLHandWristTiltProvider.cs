b7e8d1192fb0a7d186c307047b22a40d66f7e9c4
CapSparkle <Feskovich.A@yandex.com>
2021-10-27 20:47:59 +0700

    save sukablyat

--- Assets/SparkleXR/SparkleXRTemplates/Examples/Template/Scripts/MLHandWristTiltProvider.cs
+++ Assets/SparkleXR/SparkleXRTemplates/Examples/Template/Scripts/MLHandWristTiltProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,101 @@ using UnityEngine.XR.MagicLeap;
 
 namespace SparkleXRTemplates
 {
-    public class MLHandWristTiltProvider : SimpleHandInputProvider
+	public enum HandWristTilt
+	{ 
+        WristBentUp,
+        WristBentDown,
+        UndefindedTilt
+    }
+
+
+	public class MLHandWristTiltProvider : SimpleHandInputProvider
     {
+        List<Action> mySubscribers = new List<Action>();
+
+        [SerializeField]
+        MLHandTracking.HandKeyPose TiltGesture = MLHandTracking.HandKeyPose.Fist;
+
+        [SerializeField]
+        float upBentMinAngle = 15f;
+
+        [SerializeField]
+        float downBentMinAngle = 15f;
+
+
         MLHandTracking.Hand handDevice;
-        // Start is called before the first frame update
+
+        float stopTiltGestureThreshold = 0.5f; //seconds till gesture become unrecognized since wrong key pose recognition
+
+        float tiltAngle = 0f;
+        
+        Pose tiltStartPose = Pose.identity;
+
+        HandWristTilt _wristTilt = HandWristTilt.UndefindedTilt;
+        public HandWristTilt wristTilt
+        {
+            set
+			{
+                //(handDevice.Middle.MCP.Position - handDevice.Wrist.Center.Position)
+
+            }
+
+            get
+			{
+                return _wristTilt;
+            }
+            
+        }
+
         void Start()
         {
             base.Start();
 
-            if (handedness == Handedness.Right)
-                handDevice = MLHandTracking.Right;
-            else if (handedness == Handedness.Left)
-                handDevice = MLHandTracking.Left;
+            try
+			{
+                if (handedness == Handedness.Right)
+                    handDevice = MLHandTracking.Right;
+                else if (handedness == Handedness.Left)
+                    handDevice = MLHandTracking.Left;
+            }
+            catch (Exception exc)
+			{
+                print(exc.Message);
+			}
         }
 
-        // Update is called once per frame
+        void RecognizeGesture()
+		{
+            if (tiltStartPose == Pose.identity)
+            {
+                if (handDevice.KeyPose == TiltGesture)
+                {
+                    //Gesture started
+                }
+            }
+            else
+			{
+                if (handDevice.KeyPose == TiltGesture)
+                {
+
+                }
+                else
+				{
+                    if (handDevice.KeyPose == TiltGesture)
+                    {
+                        //Gesture longer presented
+                    }
+                }
+            }
+
+		}
+        
         void Update()
         {
-
+            if(mySubscribers.Count != 0)
+			{
+                
+			}
         }
     }
 }
